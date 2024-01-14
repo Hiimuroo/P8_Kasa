b29e19c80825551f5c5e5039b09a45e04cb8b78d
Hiimuroo <matthieu.labille@gmail.com>
2024-01-14 11:45:26 +0100

    modified:   package-lock.json 	modified:   package.json 	modified:   src/App.css 	modified:   src/App.js 	deleted:    src/App.test.js 	new file:   src/components/About.js 	new file:   src/components/Home/Home.js 	modified:   src/index.js 	deleted:    src/reportWebVitals.js 	deleted:    src/setupTests.js

---
+++ src/components/About.js
@@ -0,0 +1,8 @@
+// src/components/About.js
+import React from 'react';
+
+function About() {
+  return <h2>À propos</h2>;
+}
+
+export default About;
---
+++ src/components/Home/Home.js
@@ -0,0 +1,10 @@
+// src/components/Home.js
+import React from 'react';
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Bienvenue sur le site de test</h1>
+    </div>
+  )
+}
